024ce52e9357c09f16c44b39da02bd4f54148b5f
Burke Holland <burkeholland@gmail.com>
2015-05-25 10:34:46 -0500

    remove bundler

--- webpack-demo/Global.asax.cs
+++ webpack-demo/Global.asax.cs
@@ -15,7 +15,6 @@ namespace webpack_demo
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
